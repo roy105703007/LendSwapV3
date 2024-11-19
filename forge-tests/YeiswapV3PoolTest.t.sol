66bb0357dfda784d81989c96d2f371b2804f3436
roy105703007 <roy990427@gmail.com>
2024-11-19 15:13:03 +0800

    test: adding mint, burn test

--- forge-tests/YeiswapV3PoolTest.t.sol
+++ forge-tests/YeiswapV3PoolTest.t.sol
@@ -6,7 +6,8 @@ import 'forge-std/Test.sol';
 import '../contracts/YeiswapV3Factory.sol';
 import '../contracts/YeiswapV3Pool.sol';
 import '../contracts/MockERC20.sol';
-
+// help me import 'TickMath.sol'
+import '../contracts/libraries/TickMath.sol';
 contract YeiswapV3PoolTest is Test, IUniswapV3MintCallback {
     YeiswapV3Factory public factory;
     YeiswapV3Pool public pool;
@@ -38,9 +39,15 @@ contract YeiswapV3PoolTest is Test, IUniswapV3MintCallback {
     }
 
     function testAddAndRemoveLiquidity() public {
+        getPoolState();
+        getTokenBalances();
         // Define the tick range for liquidity
         int24 tickLower = -600;
         int24 tickUpper = 600;
+        uint160 tickLowerSqrtPriceX96 = TickMath.getSqrtRatioAtTick(tickLower);
+        uint160 tickUpperSqrtPriceX96 = TickMath.getSqrtRatioAtTick(tickUpper);
+        console.log('TickLower sqrtPriceX96:', uint256(tickLowerSqrtPriceX96));
+        console.log('TickUpper sqrtPriceX96:', uint256(tickUpperSqrtPriceX96));
         uint128 liquidity = 1_000_000;
 
         // Add liquidity to the pool
@@ -49,6 +56,8 @@ contract YeiswapV3PoolTest is Test, IUniswapV3MintCallback {
         // Verify the amounts of token0 and token1 added to the pool
         assertGt(amount0, 0, 'Amount0 should be greater than 0');
         assertGt(amount1, 0, 'Amount1 should be greater than 0');
+        console.log('Amount0 added:', amount0);
+        console.log('Amount1 added:', amount1);
 
         // Remove a portion of liquidity from the pool
         uint128 liquidityToRemove = liquidity / 2;
@@ -60,6 +69,32 @@ contract YeiswapV3PoolTest is Test, IUniswapV3MintCallback {
         // Verify the amounts of token0 and token1 removed from the pool
         assertGt(amount0Burned, 0, 'Amount0 burned should be greater than 0');
         assertGt(amount1Burned, 0, 'Amount1 burned should be greater than 0');
+        console.log('Amount0 burned:', amount0Burned);
+        console.log('Amount1 burned:', amount1Burned);
+
+        getPoolState();
+        getTokenBalances();
+    }
+
+    function getPoolState() public view returns (uint160 sqrtPriceX96, int24 tick, uint128 liquidity) {
+        // Retrieve the slot0 data from the pool
+        (sqrtPriceX96, tick, , , , , ) = pool.slot0();
+        // Retrieve the liquidity data from the pool
+        liquidity = pool.liquidity();
+
+        console.log('Current sqrtPriceX96:', uint256(sqrtPriceX96));
+        console.log('Current tick:', tick);
+        console.log('Current liquidity:', uint256(liquidity));
+    }
+
+    function getTokenBalances() public view returns (uint256 balanceTokenA, uint256 balanceTokenB) {
+        // Retrieve token balances in the pool
+        balanceTokenA = tokenA.balanceOf(address(pool));
+        balanceTokenB = tokenB.balanceOf(address(pool));
+
+        // Log the balances for debugging
+        console.log('TokenA balance in pool:', balanceTokenA);
+        console.log('TokenB balance in pool:', balanceTokenB);
     }
 
     function uniswapV3MintCallback(uint256 amount0Owed, uint256 amount1Owed, bytes calldata) external override {
